a4b88fc4ecafd923e9b36098e66054864a882257
DeathVenom54 <fdragon602@gmail.com>
2021-12-09 08:22:57 +0530

    change url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Affiliate Magnet',
   tagline: 'Earn from your community using affiliate links',
-  url: 'https://bots.deathvenom.me',
+  url: 'https://affiliate-magnet.deathvenom.me',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
