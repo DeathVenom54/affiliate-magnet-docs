76dafa2f7478ef0c55556c5c4aac97b4d374ef2a
DeathVenom54 <fdragon602@gmail.com>
2022-01-02 16:49:22 +0530

    add logos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,26 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={'Home'}
-      description="Replace Unity Assetstore links with your affiliate">
+      description="Replace links with affiliate ones">
         <div className="header">
           <div className="left">
             <img className="logo" src="/img/logo/mega-mascot-happy.png" alt="Affiliate Magnet logo"/>
           </div>
           <div className="right">
             <h1>Affiliate Magnet</h1>
-            <p>Monetise your Unity Discord server using Unity affiliate links</p>
+            <p>Monetise your Unity Discord server using affiliate links</p>
             <div className="buttons">
               <a href="/docs/invite">Invite</a>
               <a href="/docs/intro">Guide</a>
+            </div>
+            <div className="supports">
+              <p>Currently supports:</p>
+              <a href="https://assetstore.unity.com" target="_blank">
+                <img className="unity-logo" src="https://i.redd.it/tu3gt6ysfxq71.png" alt="Unity logo"/>
+              </a>
+              <a href="https://humblebundle.com" target="_blank">
+                <img className="humble-logo" src="https://upload.wikimedia.org/wikipedia/commons/1/1a/Humble_Bundle_H_logo_red.svg" alt="Humble Bundle logo"/>
+              </a>
             </div>
           </div>
         </div>
@@ -51,11 +60,11 @@ export default function Home(): JSX.Element {
 
         <div className="feature">
           <DiscordChat username="Timps" role="#3cd600" avatar="https://cdn.discordapp.com/avatars/209612068989566976/af4d0321a60b886ac3a779e30bd3e4bc.webp?size=80">
-            <span>Woohoo there's a big discount on <a>https://assetstore.unity.com/packages/example/some-asset-1337<span className="highlight">?aid=someOtherAffiliate</span></a> !
+            <span>This bundle looks very useful <a>https://www.humblebundle.com/books/some-imaginary-bundle<span className="highlight">?partner=someOtherPartner</span></a>
             </span>
           </DiscordChat>
 
-          <p>Someone sends an <a href="https://unity.com/affiliates">affiliate</a> link in your server...</p>
+          <p>Someone sends an <a href="https://www.humblebundle.com/partner">affiliate</a> link in your server...</p>
         </div>
 
         <div className="feature">
@@ -63,7 +72,7 @@ export default function Home(): JSX.Element {
           <p>The bot cleans it out!</p>
 
           <DiscordChat username="Affiliate Magnet" role="#ffc20c" avatar="	https://cdn.discordapp.com/avatars/891700586288934952/777719b8f803da890b4dfeb5cebbb9c5.webp?size=80">
-            <span>From <span className="mention">@Timps</span>: <br />Woohoo there's a big discount on <a>https://assetstore.unity.com/packages/example/some-asset-1337</a> !
+            <span>From <span className="mention">@Timps</span>: <br />This bundle looks very useful <a>https://www.humblebundle.com/books/some-imaginary-bundle<span className="highlight">?partner=someOtherPartner</span></a>
             </span>
           </DiscordChat>
         </div>
