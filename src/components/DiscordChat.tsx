afb4ee7be7fd654dc7584291b4bc19845c66fce7
DeathVenom54 <fdragon602@gmail.com>
2022-01-08 15:25:03 +0530

    redone css

--- src/components/DiscordChat.tsx
+++ src/components/DiscordChat.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {FC} from "react";
+import styles from './DiscordChat.module.css'
 
 interface DiscordChatProps {
   username: string;
@@ -11,14 +12,14 @@ const DiscordChat: FC<DiscordChatProps> =  (props) => {
   const now = new Date()
   const time = `${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`
 
-  return <div className="discord">
-    <img src={props.avatar || "https://cdn.discordapp.com/avatars/289677961349824512/f382f399679d9f0aa5d94aada201eef9.webp?size=80"} alt=""/>
-    <div className="message">
+  return <div className={styles.discord}>
+    <img className={styles.avatar} src={props.avatar || "https://cdn.discordapp.com/avatars/289677961349824512/7d84502d8d56d7fb96ec9cf81b261d4e.webp?size=80"} alt=""/>
+    <div className={styles.message}>
       <div>
-        <span className="username" style={{ color: props.role || '#fff' }}>{props.username}</span>
-        <span className="time">Today at {time}</span>
+        <span className={styles.username} style={{ color: props.role || '#fff' }}>{props.username}</span>
+        <span className={styles.time}>Today at {time}</span>
       </div>
-      <pre className="content">{props.children}</pre>
+      <pre className={styles.content}>{props.children}</pre>
     </div>
   </div>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import DiscordChat from "@site/src/components/DiscordChat";
+import styles from './index.module.css'
+import discord from '../components/DiscordChat.module.css'
 
 export default function Home(): JSX.Element {
   return (
     <Layout
       title={'Home'}
       description="Monetise your Discord server using affiliate links">
-        <div className="header">
-          <div className="left">
-            <img className="logo" src="/img/logo/mega-mascot-happy.png" alt="Affiliate Magnet logo"/>
-          </div>
+        <div className={styles.header}>
+          <img className={styles.logo} src="/img/logo/mega-mascot-happy.png" alt="Affiliate Magnet logo"/>
+
           <div className="right">
-            <h1>Affiliate Magnet</h1>
-            <p>Monetise your Discord server using affiliate links</p>
+            <h1 className={styles.title}>Affiliate Magnet</h1>
+            <p className={styles.description}>Monetise your Discord server using affiliate links</p>
             <div className="buttons">
-              <a href="/docs/invite">Invite</a>
-              <a href="/docs/intro">Guide</a>
+              <a className={styles.button} href="/docs/invite">Invite</a>
+              <a className={styles.button} href="/docs/intro">Guide</a>
             </div>
             <div className="supports">
-              <p>Currently supports:</p>
+              <p className={styles.supports_text}>Currently supports:</p>
               <a href="https://assetstore.unity.com" target="_blank">
-                <img className="unity-logo" src="https://i.redd.it/tu3gt6ysfxq71.png" alt="Unity logo"/>
+                <img className={styles.supports_icon} src="https://i.redd.it/tu3gt6ysfxq71.png" alt="Unity logo"/>
               </a>
               <a href="https://humblebundle.com" target="_blank">
-                <img className="humble-logo" src="https://upload.wikimedia.org/wikipedia/commons/1/1a/Humble_Bundle_H_logo_red.svg" alt="Humble Bundle logo"/>
+                <img className={styles.supports_icon} src="https://upload.wikimedia.org/wikipedia/commons/1/1a/Humble_Bundle_H_logo_red.svg" alt="Humble Bundle logo"/>
               </a>
-              <a href="/docs/faq#will-the-bot-support-any-other-websites">...and more to come</a>
+              <a href="/docs/faq#will-the-bot-support-any-other-websites" className={styles.supports_link}>...and more to come</a>
             </div>
           </div>
         </div>
@@ -34,46 +35,46 @@ export default function Home(): JSX.Element {
       <hr/>
 
       <div className="features">
-        <h1>What does it do?</h1>
+        <h1 className={styles.feature_title1}>What does it do?</h1>
 
-        <h2>When set to Replace mode:</h2>
+        <h2 className={styles.feature_title2}>When set to Replace mode:</h2>
 
-        <div className="feature">
+        <div className={styles.feature}>
           <DiscordChat username="DV" role="#47a9ff">
-            <span>I bought this asset recently: <a>https://assetstore.unity.com/packages/example/some-asset-1337</a>,<br />
+            <span>I bought this asset recently: <a className={discord.link}>https://assetstore.unity.com/packages/example/some-asset-1337</a>,<br />
             and I must say, it's amazing to work with!</span>
           </DiscordChat>
 
-          <p>Someone sends a <a href="https://assetstore.unity.com/">Unity Asset Store</a> link in your server...</p>
+          <p className={styles.feature_text}>Someone sends a <a href="https://assetstore.unity.com/">Unity Asset Store</a> link in your server...</p>
         </div>
 
-        <div className="feature">
+        <div className={styles.feature}>
 
-          <p>The bot replaces it with an affiliate link</p>
+          <p className={styles.feature_text}>The bot replaces it with an affiliate link</p>
 
           <DiscordChat username="Affiliate Magnet" role="#ffc20c" avatar="	https://cdn.discordapp.com/avatars/891700586288934952/777719b8f803da890b4dfeb5cebbb9c5.webp?size=80">
-            <span>From <span className="mention">@DV</span>: <br />I bought this asset recently: <a>https://assetstore.unity.com/packages/example/some-asset-1337<span className="highlight">?aid=exampleAffiliateCode</span></a>,<br />
+            <span>From <span className={discord.mention}>@DV</span>: <br />I bought this asset recently: <a className={discord.link}>https://assetstore.unity.com/packages/example/some-asset-1337<span className={discord.highlight}>?aid=exampleAffiliateCode</span></a>,<br />
             and I must say, it's amazing to work with!</span>
           </DiscordChat>
         </div>
 
-        <h2>When set to Clear mode:</h2>
+        <h2 className={styles.feature_title2}>When set to Clear mode:</h2>
 
-        <div className="feature">
+        <div className={styles.feature}>
           <DiscordChat username="Timps" role="#3cd600" avatar="https://cdn.discordapp.com/avatars/209612068989566976/af4d0321a60b886ac3a779e30bd3e4bc.webp?size=80">
-            <span>This bundle looks very useful <a>https://www.humblebundle.com/books/some-imaginary-bundle<span className="highlight">?partner=someOtherPartner</span></a>
+            <span>This bundle looks very useful <a className={discord.link}>https://www.humblebundle.com/books/some-imaginary-bundle<span className={discord.highlight}>?partner=someOtherPartner</span></a>
             </span>
           </DiscordChat>
 
-          <p>Someone sends an <a href="https://www.humblebundle.com/partner">affiliate</a> link in your server...</p>
+          <p className={styles.feature_text}>Someone sends an <a href="https://www.humblebundle.com/partner">affiliate</a> link in your server...</p>
         </div>
 
-        <div className="feature">
+        <div className={styles.feature}>
 
-          <p>The bot cleans it out!</p>
+          <p className={styles.feature_text}>The bot cleans it out!</p>
 
           <DiscordChat username="Affiliate Magnet" role="#ffc20c" avatar="	https://cdn.discordapp.com/avatars/891700586288934952/777719b8f803da890b4dfeb5cebbb9c5.webp?size=80">
-            <span>From <span className="mention">@Timps</span>: <br />This bundle looks very useful <a>https://www.humblebundle.com/books/some-imaginary-bundle<span className="highlight">?partner=someOtherPartner</span></a>
+            <span>From <span className={discord.mention}>@Timps</span>: <br />This bundle looks very useful <a className={discord.link}>https://www.humblebundle.com/books/some-imaginary-bundle<span className="highlight">?partner=someOtherPartner</span></a>
             </span>
           </DiscordChat>
         </div>
